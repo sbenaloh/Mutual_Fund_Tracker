f64868f72f40b571b9569e42a90ea8f93fd09dc8
sbenaloh <Sbenaloh@outlook.com>
2016-03-22 11:46:56 -0700

    Uses Yahoo Finance API to print to a file multiple stocks and their prices multiple times over a period of time

--- src/TestingMain.java
+++ src/TestingMain.java
@@ -12,18 +12,6 @@ import java.math.*;
 
 public class TestingMain {
     public static void main(String[] args) throws FileNotFoundException, InterruptedException {
-        MyStock s1 = new MyStock("MSFT", "Microsoft", 18.00);
-        System.out.println(s1);
-        File outputFile = new File("MyStock.txt");
-        PrintStream output = new PrintStream(outputFile);
-        output.println(s1 + " 100");
-        output.println(new MyStock("APPL", "Apple", 22.50) + " 92");
-        output.println(new MyStock("JHNS", "Johnson&Johnson", 15.25) + " 500");
-
-        Fund fund1 = new Fund("TICRX", "TIAA", 17.14, new Scanner(outputFile));
-        System.out.println();
-        fund1.printHoldings(System.out);
-
         try {
             Stock s2 = YahooFinance.get("^IXIC");
             File testFile = new File("testFile.txt");
@@ -39,8 +27,6 @@ public class TestingMain {
                         .map(TestingMain::getStockQuote)
                         .map(StockQuote::getPrice)
                         .forEach(testStream::println);
-//                StockQuote quote = s2.getQuote(true);
-//                testStream.println(quote.getPrice());
                 Thread.sleep(5000);
             }
         } catch (IOException e) {
